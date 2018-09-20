c9e640d43857287d778be95313c5f893787f7f6f
Justin Huang <justinh7@illinois.edu>
2018-09-19 22:50:07 -0500

    Finished multiply by 3 and concatenate

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function multBy3(nums) {
-  let newNums = [];
-  for (int i = 0; i < nums.length; i++) {
-    newNums.push(nums[i] * 3);
-  }
+  let newNums = nums.map(elem => elem * 3);
   return newNums;
 }
+
+function concat(strs) {
+  let str = strs.reduce((prev, elem) => prev + elem);
+  return str;
+}
+
+console.log(multBy3([1, 2, 3, 4, 5]));
+console.log(concat(["a", "b", "c", "d", "e"]));
